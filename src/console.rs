adcaf7f5438baa1837a88a7dbc859c0817366628
COnion505 <shionkasagi505@gmail.com>
2023-05-25 07:29:53 +0900

    format

--- src/console.rs
+++ src/console.rs
@@ -1,26 +1,27 @@
+use crate::common::{input, Content};
 use std::collections::HashMap;
-use crate::common::{ Content,input };
 
-pub fn run(data: Vec<Content>) -> Option<i32>{
-	
+pub fn run(data: Vec<Content>) -> Option<i32> {
     // 入力結果保持用変数
     let mut received_answer = HashMap::new();
     //データ数分ループ
-    for (i,d) in data.iter().enumerate() {
-        let no = i+1;
+    for (i, d) in data.iter().enumerate() {
+        let no = i + 1;
         //インデックスから問題番号表示
-        println!("No.{}",no);
+        println!("No.{}", no);
         //問題文表示
-        println!("{}",d.text);
+        println!("{}", d.text);
         //選択肢表示
-        let mut selects_vec: Vec<(&String,&String)> = d.selects.iter().collect();
+        let mut selects_vec: Vec<(&String, &String)> = d.selects.iter().collect();
         selects_vec.sort();
-        for (k,v) in selects_vec{
+        for (k, v) in selects_vec {
             println!("{}: {}", k, v);
         }
         //入力受付
         let answer = input(">>").to_uppercase();
-        if answer == "Q" {break;}
+        if answer == "Q" {
+            break;
+        }
         received_answer.insert(i, answer);
         println!("------------------------------------------------------------------");
     }
@@ -31,18 +32,23 @@ pub fn run(data: Vec<Content>) -> Option<i32>{
     for (k, v) in sorted_recieved_answer {
         //正誤判定(contentのanswerの文字列と入力結果の文字列を比較するだけ)
         let result = if data.get(*k).unwrap().answer == v.clone() {
-            correct_count +=1;
+            correct_count += 1;
             "o"
         } else {
             "x"
         };
         //問題番号:入力値 -> 正解 o/x の形式で各問題結果出力
-        println!("No.{}: {} -> {} [{}]", k+1, v, data.get(*k).unwrap().answer, result);
+        println!(
+            "No.{}: {} -> {} [{}]",
+            k + 1,
+            v,
+            data.get(*k).unwrap().answer,
+            result
+        );
     }
     //正誤集計出力
     println!("{}/{}", correct_count, received_answer.len());
     input("終了するには何かキーを入力してください....");
 
-	Some(0)
-
+    Some(0)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod common;
 mod console;
-use common::{ input_content };
+use common::input_content;
 
 // main
-fn main(){
-    input_content()
-    .and_then(console::run)
-    .expect("can't run.");
+fn main() {
+    input_content().and_then(console::run).expect("can't run.");
 }
-
