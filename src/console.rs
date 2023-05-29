6589023eddcd1e5f43a18a579d8198dc7996b1df
COnion505 <55478189+COnion505@users.noreply.github.com>
2023-05-29 23:52:03 +0900

    コメント修正

--- src/console.rs
+++ src/console.rs
@@ -1,24 +1,21 @@
 use crate::common::{input, Content};
 use std::collections::HashMap;
 
+/// コンソールアプリのメイン処理を実行する
 pub fn run(data: Vec<Content>) -> Option<i32> {
-    // 入力結果保持用変数
-    let mut received_answer = HashMap::new();
-    //データ数分ループ
+    let mut received_answer: HashMap<usize, String> = HashMap::new();
     for (i, d) in data.iter().enumerate() {
-        let no = i + 1;
-        //インデックスから問題番号表示
+        let no: usize = i + 1;
+
         println!("No.{}", no);
-        //問題文表示
         println!("{}", d.text);
-        //選択肢表示
+
         let mut selects_vec: Vec<(&String, &String)> = d.selects.iter().collect();
         selects_vec.sort();
         for (k, v) in selects_vec {
             println!("{}: {}", k, v);
         }
-        //入力受付
-        let answer = input(">>").to_uppercase();
+        let answer = input("input(Q: quit)>>").to_uppercase();
         if answer == "Q" {
             break;
         }
@@ -30,14 +27,12 @@ pub fn run(data: Vec<Content>) -> Option<i32> {
     let mut sorted_recieved_answer: Vec<(&usize, &String)> = received_answer.iter().collect();
     sorted_recieved_answer.sort();
     for (k, v) in sorted_recieved_answer {
-        //正誤判定(contentのanswerの文字列と入力結果の文字列を比較するだけ)
         let result = if data.get(*k).unwrap().answer == v.clone() {
             correct_count += 1;
             "o"
         } else {
             "x"
         };
-        //問題番号:入力値 -> 正解 o/x の形式で各問題結果出力
         println!(
             "No.{}: {} -> {} [{}]",
             k + 1,
@@ -46,7 +41,6 @@ pub fn run(data: Vec<Content>) -> Option<i32> {
             result
         );
     }
-    //正誤集計出力
     println!("{}/{}", correct_count, received_answer.len());
     input("終了するには何かキーを入力してください....");
 
